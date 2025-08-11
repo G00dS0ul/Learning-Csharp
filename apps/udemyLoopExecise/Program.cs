f4f9218e0aebadba683a0facdfe0306067df822a
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-08-11 06:37:43 +0100

    Playing with While Loop to allow user continue to enter number in the console and only terminate only if the user enters OK and display the sum of all the number the user entered.

---
+++ apps/udemyLoopExecise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace udemyLoopExecise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initilize the sum to 0
+            int sum = 0;
+            while (true)
+            {
+                Console.Write("Enter Your Number or type OK to exit: ");
+                var input = Console.ReadLine();
+                var exitLoop = "OK"; 
+                if(input == exitLoop)  //Condition to exit the loop
+                {
+                    break;
+                }
+                
+                if (int.TryParse(input, out var number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input. Please enter a valid number or enter OK to exit: ");
+                }
+                
+            }
+            Console.WriteLine($"Sum of number entered: {sum}");
+
+                
+            
+        }
+    }
+}
