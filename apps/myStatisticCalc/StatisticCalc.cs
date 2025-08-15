880d37cf8afbb7cfeb23db88f76e04370c12a82b
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-08-15 19:46:13 +0100

    added an exit condition to the statistic calculator

--- apps/myStatisticCalc/StatisticCalc.cs
+++ apps/myStatisticCalc/StatisticCalc.cs
@@ -11,47 +11,55 @@ namespace myStatisticCalc
     {
         static void Main(string[] args)
         {
-            //Allow user to input data and store in input
-            Console.Write("Enter Your Data: ");
-            string input = Console.ReadLine();
-            //allow user to input multiple data seperated by a space and spliting it into an array and empty entries are removed
-            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            double[] data = new double[parts.Length];
-            //the new data created is then converted to a double and if the conversion is unsuccefull the value will result 0
-            for (int i = 0; i < parts.Length; i++)
+            while (true)
             {
-                double.TryParse(parts[i], out data[i]);
-            }
-            //allow user to input the type of statistic operation to do 
-            Console.Write("Enter your Statistics Operation: ");
-            string opdata = Console.ReadLine();
-            //allow user to pass more than one operation also seperated by a space
-            string[] inputop = opdata.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            //used the switch method to check which kind of operation to execute in order and called up the method to execute on terminal
-            foreach(string op in inputop)
-            {
-                switch (op)
+                //Allow user to input data and store in input
+                Console.Write("Enter Your Data: ");
+                string input = Console.ReadLine();
+                //allow user to input multiple data seperated by a space and spliting it into an array and empty entries are removed
+                string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                double[] data = new double[parts.Length];
+                //Condition to exit
+                if (input == "done" || string.IsNullOrEmpty(input))
                 {
-                    case "sum":
-                        Console.WriteLine("The Sum of the Data is: " + SumOfData(data));
-                        break;
-                    case "avg":
-                        Console.WriteLine("The Mean of the Data is: " + MeanOfData(data));
-                        break;
-                    case "min":
-                        Console.WriteLine("The Minimum Value of the Data is: " + MinNumber(data));
-                        break;
-                    case "max":
-                        Console.WriteLine("The Maximum Value of the Data is: " + MaxNumber(data));
-                        break;
+                    Console.WriteLine("Program Terminate");
+                    break;
                 }
+                //the new data created is then converted to a double and if the conversion is unsuccefull the value will result 0
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    double.TryParse(parts[i], out data[i]);
+                }
+                //allow user to input the type of statistic operation to do 
+                Console.Write("Enter your Statistics Operation*(sum, avg, min, max): ");
+                string opdata = Console.ReadLine();
+                //allow user to pass more than one operation also seperated by a space
+                string[] inputop = opdata.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                //used the switch method to check which kind of operation to execute in order and called up the method to execute on terminal
+                foreach (string op in inputop)
+                {
+                    switch (op)
+                    {
+                        case "sum":
+                            Console.WriteLine("The Sum of the Data is: " + SumOfData(data));
+                            break;
+                        case "avg":
+                            Console.WriteLine("The Mean of the Data is: " + MeanOfData(data));
+                            break;
+                        case "min":
+                            Console.WriteLine("The Minimum Value of the Data is: " + MinNumber(data));
+                            break;
+                        case "max":
+                            Console.WriteLine("The Maximum Value of the Data is: " + MaxNumber(data));
+                            break;
+                    }
+                }
+
+
             }
 
 
-
-
-
-            Console.ReadLine();
+                Console.ReadLine();
         }
         //Created methods of For-loops to calculate each operation
         //for summation of data method
@@ -82,7 +90,7 @@ namespace myStatisticCalc
             double min = data[0];
             for(int i = 0; i < data.Length; i++)
             {
-                if (min < data[i])
+                if (data[i] < min)
                 {
                     min = data[i];
                 }
@@ -96,7 +104,7 @@ namespace myStatisticCalc
             double max = data[0];
             for (int i = 0; i < data.Length; i++)
             {
-                if (max > data[i])
+                if (data[i] > max)
                 {
                     max = data[i];
                 }
