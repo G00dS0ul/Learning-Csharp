7358a1a7f9795e6e9430a3720c50b5131e7b5300
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-08-26 01:47:52 +0100

    Included Auto-Implemeted properties, also made use of my class knowledge to make my code robust and also included more Statistic operation

---
+++ apps/myStatisticCalc/Program.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myStatisticCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] previousData = null; //store the previous data
+            while (true)
+            {
+                Console.Write("Enter Your Data: ");
+                string input = Console.ReadLine();
+
+                double[] data;
+               
+                //Condition to exit
+                if (input == "done" || string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Program Terminate");
+                    break;
+                }
+                if (input == "recall")
+                {
+                    if (previousData == null)
+                    {
+                        Console.WriteLine( "No previous Data to recall");
+                        continue;
+                    }
+                    else
+                    {
+                        data = previousData;
+                        Console.WriteLine("Previous Data Inputed is: " + string.Join(" ", data));
+                    }
+                        
+                }
+                else
+                {
+                    //allow user to input multiple data seperated by a space and spliting it into an array and empty entries are removed
+                    string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    data = new double[parts.Length];
+                    //the new data created is then converted to a double and if the conversion is unsuccefull the value will result 0
+                    for (int i = 0; i < parts.Length; i++)
+                    {
+                        double.TryParse(parts[i], out data[i]);
+                    }
+                    previousData = data; //save current data as previous
+                }
+                    
+                //allow user to input the type of statistic operation to do 
+                Console.Write("Enter your Operation: ");
+                string opdata = Console.ReadLine();
+                //allow user to pass more than one operation also seperated by a space
+                string[] inputop = opdata.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                //used the switch method to check which kind of operation to execute in order and called up the method to execute on terminal
+
+                var statCalc = new StatisticCalc();
+                foreach (string op in inputop)
+                {
+                    switch (op)
+                    {
+                        case "sum":
+                            statCalc.Sum = StatisticCalc.SumOfData(data);
+                            Console.WriteLine("The Sum of the Data is: " + statCalc.Sum);
+                            break;
+                        case "avg":
+                            statCalc.Avg = StatisticCalc.MeanOfData(data);
+                            Console.WriteLine("The Mean of the Data is: " + statCalc.Avg);
+                            break;
+                        case "min":
+                            statCalc.Min = StatisticCalc.MinNumber(data);
+                            Console.WriteLine("The Minimum Value of the Data is: " + statCalc.Min);
+                            break;
+                        case "max":
+                            statCalc.Max = StatisticCalc.MaxNumber(data);
+                            Console.WriteLine("The Maximum Value of the Data is: " + statCalc.Max);
+                            break;
+                        case "mode":
+                            double[] modes = StatisticCalc.ModeOfData(data);
+                            Console.WriteLine("The Highest frequency of the Data is: " + string.Join(" ", modes));
+                            break;
+                        case "var":
+                            statCalc.Var = StatisticCalc.VarianceOfData(data);
+                            Console.WriteLine("The Sample variance of the data is: " + statCalc.Var);
+                            break;
+                        case "SD":
+                            statCalc.SD = StatisticCalc.StandardDeviationOfData(data);
+                            Console.WriteLine("The Standard Devition of the data is: " + statCalc.SD);
+                            break;
+                        case "med":
+                            statCalc.Med = StatisticCalc.MedianOfData(data);
+                            Console.WriteLine("The Median of the data is: " + statCalc.Med);
+                            break;
+                    }
+                }
+
+
+            }
+
+
+        }
+      
+    }
+}
--- apps/myStatisticCalc/StatisticCalc.cs
+++ apps/myStatisticCalc/StatisticCalc.cs
@@ -1,94 +1,72 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace myStatisticCalc
+﻿namespace myStatisticCalc
 {
-    class StatisticCalc
+    public class StatisticCalc
     {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                //Allow user to input data and store in input
-                Console.Write("Enter Your Data: ");
-                string input = Console.ReadLine();
-                //allow user to input multiple data seperated by a space and spliting it into an array and empty entries are removed
-                string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                double[] data = new double[parts.Length];
-                //Condition to exit
-                if (input == "done" || string.IsNullOrEmpty(input))
-                {
-                    Console.WriteLine("Program Terminate");
-                    break;
-                }
-                //the new data created is then converted to a double and if the conversion is unsuccefull the value will result 0
-                for (int i = 0; i < parts.Length; i++)
-                {
-                    double.TryParse(parts[i], out data[i]);
-                }
-                //allow user to input the type of statistic operation to do 
-                Console.Write("Enter your Statistics Operation*(sum, avg, min, max): ");
-                string opdata = Console.ReadLine();
-                //allow user to pass more than one operation also seperated by a space
-                string[] inputop = opdata.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                //used the switch method to check which kind of operation to execute in order and called up the method to execute on terminal
-                foreach (string op in inputop)
-                {
-                    switch (op)
-                    {
-                        case "sum":
-                            Console.WriteLine("The Sum of the Data is: " + SumOfData(data));
-                            break;
-                        case "avg":
-                            Console.WriteLine("The Mean of the Data is: " + MeanOfData(data));
-                            break;
-                        case "min":
-                            Console.WriteLine("The Minimum Value of the Data is: " + MinNumber(data));
-                            break;
-                        case "max":
-                            Console.WriteLine("The Maximum Value of the Data is: " + MaxNumber(data));
-                            break;
-                    }
-                }
+        //the use of Auto-implemented property
+        public double Sum { get; set; }
+        public double Avg { get; set; }
+        public double Max { get; set; }
+        public double Min { get; set; }
+        public double Mode { get; set; }
+        public double Med { get; set; }
+        public double Var { get; set; }
+        public double SD { get; set; }
 
-
-            }
-
-
-                Console.ReadLine();
-        }
-        //Created methods of For-loops to calculate each operation
-        //for summation of data method
-        static double SumOfData(double[] data)
+       
+        public static double SumOfData(double[] data)
         {
             double sum = 0;
-            for(int i = 0; i <data.Length; i++)
+            for (var i = 0; i < data.Length; i++)
             {
                 sum += data[i];
             }
             return sum;
         }
 
-        //for average calculation method
-        static double MeanOfData(double[] data)
+        public static double MeanOfData(double[] data)
         {
             double mean = 0;
-            for (int i = 0; i < data.Length; i++)
+            for (var i = 0; i < data.Length; i++)
             {
                 mean += data[i] / data.Length;
             }
             return mean;
         }
 
-        //for minimum value method
-        static double MinNumber(double[] data)
+        public static double VarianceOfData(double[] data)
+        {
+            double mean = MeanOfData(data);
+            double variance = 0;
+            for(var i = 0; i < data.Length; i++)
+            {
+                variance += Math.Pow(data[i] - mean, 2) / (data.Length - 1); 
+            }
+            return variance;
+        }
+
+        public static double StandardDeviationOfData(double[] data)
+        {
+            double variance = VarianceOfData(data);
+            double standardDeviation = Math.Sqrt(variance);
+            return standardDeviation;
+        }
+
+        public static double MaxNumber(double[] data)
+        {
+            double max = data[0];
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i] > max)
+                {
+                    max = data[i];
+                }
+            }
+            return max;
+        }
+        public static double MinNumber(double[] data)
         {
             double min = data[0];
-            for(int i = 0; i < data.Length; i++)
+            for (var i = 0; i < data.Length; i++)
             {
                 if (data[i] < min)
                 {
@@ -98,18 +76,47 @@ namespace myStatisticCalc
             return min;
         }
 
-        //for maximum value method
-        static double MaxNumber(double[] data)
+        public static double[] ModeOfData(double[] data)
         {
-            double max = data[0];
-            for (int i = 0; i < data.Length; i++)
+            var frequency = new Dictionary<double, int>();
+            foreach (var value in data)
             {
-                if (data[i] > max)
+                if (frequency.ContainsKey(value))
                 {
-                    max = data[i];
+                    frequency[value]++;
+                }
+                else
+                {
+                    frequency[value] = 1;
                 }
             }
-            return max;
+
+            int maxCount = frequency.Values.Max();
+            var modes = frequency.Where(pair => pair.Value == maxCount)
+                .Select(pair => pair.Key)
+                .ToArray();
+            
+            return modes;
+        }
+
+        public static double MedianOfData(double[] data)
+        {
+            if (data ==  null || data.Length == 0)
+            {
+                throw new ArgumentException("Data Array cannot be null or Empty");
+            }
+            var orderedData = data.OrderBy(x => x).ToArray();
+            int n = orderedData.Length;
+            if (n % 2 == 1) //odd numbers of data
+            {
+                return orderedData[n / 2];
+            }
+            else //even number of data
+            {
+                return (orderedData[(n / 2) - 1] + orderedData[n / 2]) / 2.0;
+            }
         }
     }
+
+
 }
