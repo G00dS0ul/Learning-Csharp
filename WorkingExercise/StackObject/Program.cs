6df0ab51dfc19a5072954f25d02c388c2875f6a5
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-09-15 23:00:52 +0100

    The stack program basically takes in element from the Console and stores it in a list of Maximum of 5 stack memory and you can also delete an element by using the Pop command and also clear all the element in the list by using the clear command

---
+++ WorkingExercise/StackObject/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace StackObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            Console.WriteLine("Input Your element you want to enter.");
+
+            while (true)
+            {
+                int initial = 0;
+                int max = 5;
+                
+                for (var i = initial; i <= max; i++)
+                {
+                    Console.Write("->");
+                    var input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("The Element is empty, Input an element");
+                        continue;
+                    }
+                        
+                    if (input.Equals("POP", StringComparison.OrdinalIgnoreCase))
+                    {
+                        try
+                        {
+                            Console.WriteLine("Element Popped: {0} ", stack.Pop());
+                        }
+
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        continue;
+                    }
+
+                    if (input.Equals("CLEAR", StringComparison.OrdinalIgnoreCase))
+                    {
+                        stack.Clear();
+                        Console.WriteLine("Stack Cleared!!!");
+                        continue;
+                    }
+
+                    if (input.Equals("Show", StringComparison.OrdinalIgnoreCase) || stack.Items.Count == max)
+                    {
+                        Console.WriteLine("Your Stack Includes: ");
+                        
+                        foreach (var items in stack.Items)
+                            Console.WriteLine(items);
+                        continue;
+                    }
+
+                    stack.Push(input);
+                }
+                
+
+                
+            }
+            
+
+           
+        }
+    }
+}
---
+++ WorkingExercise/StackObject/Stack.cs
@@ -0,0 +1,36 @@
+﻿namespace StackObject
+{
+    public class Stack
+    {
+        private readonly List<object> element = new List<object>();
+        
+
+        public void Push(object value)
+        {
+            if (value == null)
+                throw new InvalidOperationException("You cannot Add a Null elment");
+            
+                this.element.Add(value);
+        }
+
+        public object Pop()
+        {
+            if (element.Count == 0)
+                throw new InvalidOperationException("The element is empty hence, cannot display stack");
+
+            var index = element.Count - 1;
+            var display = element[index];
+            element.RemoveAt(index);
+
+            return display;
+
+        }
+
+        public void Clear()
+        {
+            this.element.Clear();
+        }
+
+        public IReadOnlyList<object> Items => element;
+    }
+}
