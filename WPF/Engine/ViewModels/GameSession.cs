35c161ec710c6f471c8026f2ce25d3d27953caee
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-12-12 23:31:22 +0100

    This is the image for the location

---
+++ WPF/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,37 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player? CurrentPlayer { get; set; }
+        public Location? CurrentLocation { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Iche",
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                Gold = 100000,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+
+            CurrentLocation = new Location
+            {
+                XCoordinate = 0,
+                YCoordinate = -1,
+                Name = "Home",
+                Description = "This is your House",
+                ImageName = "/Engine;component/Images/Locations/Home.png"
+            };
+        }
+    }
+}
