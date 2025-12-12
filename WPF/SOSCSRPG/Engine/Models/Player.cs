92b47bf2d6c6b28a4a095280eb6099e5b6ec8c4d
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-12-12 17:32:10 +0100

    the engine and model

---
+++ WPF/SOSCSRPG/Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoint { get; set; }
+        public int ExperiencePoint { get; set; }
+        public int Level { get; set; }
+        public int Gold { get;  set; }
+    }
+}
