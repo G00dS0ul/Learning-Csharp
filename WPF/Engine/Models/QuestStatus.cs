c6d7eb9865c3f9abff593d0c2ee627fba871f3bd
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-12-15 00:41:33 +0100

    Implemented the Player Quest to perform and updated and interface

---
+++ WPF/Engine/Models/QuestStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class QuestStatus
+    {
+        public Quest PlayerQuest { get; set; }
+        public bool IsComplete { get; set; }
+
+        public QuestStatus(Quest quest)
+        {
+            this.PlayerQuest = quest;
+            this.IsComplete = false;
+        }
+    }
+}
