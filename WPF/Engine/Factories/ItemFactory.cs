6ff4d162fc97b6d73d2d809fa08370067d27ade7
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-12-17 04:33:03 +0100

    Completed the monster feature and the combat feature

--- WPF/Engine/Factories/ItemFactory.cs
+++ WPF/Engine/Factories/ItemFactory.cs
@@ -13,8 +13,6 @@ namespace Engine.Factories
 
         static ItemFactory()
         {
-            _standardGameItems = new List<GameItem>();
-
             _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
             _standardGameItems.Add(new GameItem(9001, "Snake Fang", 1));
