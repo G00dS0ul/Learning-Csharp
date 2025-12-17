de3c26e0c227bc8f3606ab08ba5afaaa7bfa3bf9
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-12-17 23:18:34 +0100

    Implemented the trader class

---
+++ WPF/Engine/Factories/TraderFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class TraderFactory
+    {
+        private static readonly List<Trader> _traders = new List<Trader>();
+
+        static TraderFactory()
+        {
+            Trader innerCircle = new Trader("Inner Circle Trader");
+            innerCircle.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+
+            Trader blackFarmer = new Trader("BlackFarmer Circle");
+            blackFarmer.AddItemToInventory(ItemFactory.CreateGameItem(1002));
+
+            Trader theEmptyPocket = new Trader("The Empty Pocket Circle");
+            theEmptyPocket.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+
+            AddTraderToList(innerCircle);
+            AddTraderToList(blackFarmer);
+            AddTraderToList(theEmptyPocket);
+        }
+
+        public static Trader GetTraderByName(string name)
+        {
+            return _traders.FirstOrDefault(t => t.Name == name);
+        }
+
+        private static void AddTraderToList(Trader trader)
+        {
+            if(_traders.Any(t => t.Name == trader.Name))
+            {
+                throw new ArgumentException($"There is already a trader named '{trader.Name}'");
+            }
+
+            _traders.Add(trader);
+        }
+    }
+}
