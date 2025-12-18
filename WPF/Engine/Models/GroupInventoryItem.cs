9140244c449956f6e139ca2a72f3fdb3aef50488
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2025-12-18 06:09:51 +0100

    Still Refactoring the the project introducing the GroupedInventoryItem Class to group unique equipment

---
+++ WPF/Engine/Models/GroupInventoryItem.cs
@@ -0,0 +1,40 @@
+﻿namespace Engine.Models
+{
+    public class GameItem : BaseNotificationClass
+    {
+        private GameItem? _item;
+        private int _quantity;
+
+        public GameItem? Item
+        {
+            get 
+            { 
+                return _item; 
+            }
+            set 
+            {
+                _item = value;
+                OnPropertyChanged(nameof(Item));
+            }
+        }
+
+        public int Quantity
+        {
+            get 
+            { 
+                return _quantity; 
+            }
+            set 
+            {
+                _quantity = value;
+                OnPropertyChanged(nameof(Quantity));
+            }
+        }
+
+        public GameItem(GameItem item, int quantity)
+        {
+            this.Item = item;
+            this.Quantity = quantity;
+        }
+    }
+}
