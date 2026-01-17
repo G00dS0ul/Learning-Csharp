ff38fa8ae0672f27f2d4c359093a4950905e6cb7
Olowogoke Iseoluwa <iseoluwaolowogoke@student.oauife.edu.ng>
2026-01-17 10:29:05 +0100

    Added a complex combat logic to the game

---
+++ WPF/Engine/Services/CombatService.cs
@@ -0,0 +1,39 @@
+﻿using Engine.Models;
+
+namespace Engine.Services
+{
+    public class CombatService
+    {
+        public enum Combatant
+        {
+            Player,
+            Opponent
+        }
+
+        public static Combatant FirstAttacker(Player player, Monster opponent)
+        {
+            var playerDexterity = player.Dexterity * player.Dexterity;
+            var opponentDexterity = opponent.Dexterity * opponent.Dexterity;
+            var dexterityOffset = (playerDexterity - opponentDexterity) / 10m;
+            var randomOffset = RandomNumberGenerator.NumberBetween(-10, 10);
+            var totalOffset = dexterityOffset + randomOffset;
+
+            return RandomNumberGenerator.NumberBetween(0, 100) <= 50 + totalOffset
+                ? Combatant.Player
+                : Combatant.Opponent;
+
+        }
+
+        public static bool AttackSucceeded(LivingEntity attacker, LivingEntity target)
+        {
+            var playerDexterity = attacker.Dexterity * attacker.Dexterity;
+            var opponentDexterity = target.Dexterity * target.Dexterity;
+            var dexterityOffset = (
+                playerDexterity - opponentDexterity) / 10m;
+            var randomOffset = RandomNumberGenerator.NumberBetween(-10, 10);
+            var totalOffset = dexterityOffset + randomOffset;
+
+            return RandomNumberGenerator.NumberBetween(0, 100) <= 50 + totalOffset;
+        }
+    }
+}
