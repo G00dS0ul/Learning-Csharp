e3aa2db348e4049776fc39e789caf314e84c2ef3
NoThingHere <olowogokeiseoluwapc@gmail.com>
2026-01-21 01:59:29 +0100

    Rearranged my project to the last commit to clear some errors

---
+++ WPF/Engine/Models/GameDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Engine.Models
+{
+    public class GameDetails
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public List<PlayerAttribute> PlayerAttributes { get; set; } = [];
+        public GameDetails(string name, string version)
+        {
+            Name = name;
+            Version = version;
+        }
+    }
+}
+
